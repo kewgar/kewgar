faf5bbb5e7f14445466f4d5b232c7b3531c9762a
kewgar <tanakron_thapkun@cmu.ac.th>
2017-03-21 12:42:14 +0700

    testupload

--- upload2.php
+++ upload2.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 $target_dir = "dataset/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -41,13 +41,5 @@ if ($uploadOk == 0) {
         echo "Sorry, there was an error uploading your file.";
     }
 }
-?> -->
+?> 
 
-<?php
-$uploads_dir = './dataset'; //Directory to save the file that comes from client application.
-if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
-    $tmp_name = $_FILES["file"]["tmp_name"];
-    $name = $_FILES["file"]["name"];
-    move_uploaded_file($tmp_name, "$uploads_dir/$name");
-}
-?>
