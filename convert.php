ac20e15d9522b52828f19a0bc975aca812e40140
kewgar <tanakron_thapkun@cmu.ac.th>
2017-03-22 23:24:21 +0700

    test up

--- convert.php
+++ convert.php
@@ -1,33 +1,33 @@
 <?php
 
 
-		if (isset($_POST['user_id'])) 
+		if (isset($_GET['user_id'])) 
 		{
-		    $user_id = $_POST['user_id'];
+		    $user_id = $_GET['user_id'];
 		} 
 		else 
 		{
 		    $user_id = 29;
 		}
-		chdir('C:\Program Files\Weka-3-8');
+		chdir('/var/www/html/kewgar');
 		$create = 'if not exist "dataset" mkdir "dataset"';
 		exec($create,$output);
 
-		chdir('C:\Program Files\Weka-3-8\dataset');
+		chdir('/var/www/html/kewgar/dataset');
 		$create2 = 'if not exist "'.$user_id.'" mkdir "'.$user_id.'"';
 		exec($create2,$output);
 
-		$copy1 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s1.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s1.csv"';
-		exec($copy1,$output1);
-		$copy2 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s2.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s2.csv"';
-		exec($copy2,$output2);
-		$copy3 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s3.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s3.csv"';
-		exec($copy3,$output3);
-		$copy4 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s4.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s4.csv"';
-		exec($copy4,$output4);
+		// $copy1 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s1.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s1.csv"';
+		// exec($copy1,$output1);
+		// $copy2 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s2.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s2.csv"';
+		// exec($copy2,$output2);
+		// $copy3 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s3.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s3.csv"';
+		// exec($copy3,$output3);
+		// $copy4 = 'copy "C:\Users\kewgar\AppData\Roaming\kewgar dataset\s4.csv" "C:\Program Files\Weka-3-8\dataset\"'.$user_id.'"\s4.csv"';
+		// exec($copy4,$output4);
 
 
-		chdir('C:\Program Files\Weka-3-8');
+		// chdir('C:\Program Files\Weka-3-8');
 		//$output3 = shell_exec('dir');
 
 		//$cmd2 = "java -cp weka.jar weka.classifiers.functions.MultilayerPerceptron -t data/s1.csv ";
