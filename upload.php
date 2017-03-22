7d5eb63e82c01d49383485e3ac3ef0a9f5ad1e48
kewgar <tanakron_thapkun@cmu.ac.th>
2017-03-23 00:20:24 +0700

    test up

--- upload.php
+++ upload.php
@@ -14,10 +14,10 @@ if (isset($_GET['user_id']))
 $create2 = 'mkdir -p dataset/'.$user_id;
 exec($create2,$output);
 
-$uploads_dir = "dataset/".$user_id."/"; //Directory to save the file that comes from client application.
-if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
-    $tmp_name = $_FILES["file"]["tmp_name"];
-    $name = $_FILES["file"]["name"];
-    move_uploaded_file($tmp_name, "$uploads_dir/$name");
+// $uploads_dir = "dataset/".$user_id."/"; //Directory to save the file that comes from client application.
+// if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
+//     $tmp_name = $_FILES["file"]["tmp_name"];
+//     $name = $_FILES["file"]["name"];
+//     move_uploaded_file($tmp_name, "$uploads_dir/$name");
 }
 ?>
